6b2689898c5367ffb1f6831dfa101a6e588736ed
cinekele <cinekele@gmail.com>
2024-05-16 23:48:10 +0200

    add soft gated pose estimation

---
+++ soft_gated_pose_estimation/model.py
@@ -0,0 +1,27 @@
+import time
+import torch
+from models.stacked_hourglass import PoseNet, SGSC_PoseNet
+
+
+def load_model(config):
+    inp_dim = config['inference']['inp_dim']
+    oup_dim = config['inference']['oup_dim']
+    nstack = config['inference']['nstack']
+    architecture = config['architecture']
+
+    # default model is SGSC
+    if architecture == 'SHG':
+        net = PoseNet(nstack, inp_dim, oup_dim)
+    else:
+        net = SGSC_PoseNet(nstack, inp_dim, oup_dim)
+
+    return net
+
+
+
+def load_model_weights(config, net):
+    checkpoint_path = config['inference']['checkpoint_path']
+    checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu')) 
+    state_dict = checkpoint['model_state_dict']
+    net.load_state_dict(state_dict)
+    return net
