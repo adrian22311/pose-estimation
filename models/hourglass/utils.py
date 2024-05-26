1d108f2577f1524a0ab0d7fb6f65aa652d42aaa9
cinekele <cinekele@gmail.com>
2024-05-26 23:07:24 +0200

    fix hourglass

--- models/hourglass/utils.py
+++ models/hourglass/utils.py
@@ -7,23 +7,23 @@ def to_key_points(query_locations: np.array, **kwargs) -> tuple[list[tuple[float
     :return: tuple: the list of keypoints and the configuration of the edges - 2D coordinates of the key points in the format of (x, y) and the configuration of the edges
     """
 
-    keypoints: list[tuple[float, float]] = [(x[0], x[1]) for x in query_locations]
+    keypoints: list[tuple[float, float]] = [(x[0], x[1]) if x[0] >= 1 and x[1] >= 1 else (None, None) for x in query_locations]
     config: list[tuple[tuple[int, int], int]] = [
-        ((0, 1), 0), # head_top -> upper_neck 
-        ((1, 5), 0), # upper_neck -> thorax
-        ((5, 2), 1),  # thorax -> r_shoulder
-        ((5, 6), -1),  # thorax -> l_shoulder
-        ((5, 9), 0),  # thorax -> pelvis
-        ((2, 3), 1),  # r_shoulder -> r_elbow
-        ((3, 4), 1),  # r_elbow -> r_wrist
-        ((6, 7), -1),  # l_shoulder -> l_elbow
-        ((7, 8), -1),  # l_elbow -> l_wrist
-        ((9, 10), 1),  # pelvis -> r_hip
-        ((9, 13), -1),  # pelvis -> l_hip
-        ((10, 11), 1),  # r_hip -> r_knee
-        ((11, 12), 1), # r_knee -> r_ankle
-        ((13, 14), -1), # l_hip -> l_knee
-        ((14, 15), -1) # l_knee -> l_ankle
+        ((0, 1), 1), # rank - rkne
+        ((3, 4), -1), # lhip - lkne
+        ((4, 5), -1), # lkne - lank
+        ((8, 9), 0), # neck - head
+        ((7, 8), 0), # thrx - neck
+        ((7, 12), 0),  # thrx - rsho
+        ((7, 13), 0), # thrx - lsho
+        ((11, 12), 1), # relb - rsho
+        ((13, 14), -1), # lsho - lelb
+        ((10, 11), 1), # rwri - relb
+        ((14, 15), -1), # lelb - lwri
+        ((6, 7), 0), # pelv - thrx
+        ((2, 6), 0), # rhip - pelv
+        ((3, 6), 0), # lhip - pelv
+        ((1, 2), 1), # rkne - rhip
     ]
     return keypoints, config
 
@@ -55,7 +55,7 @@ def to_pckh(query_locations: np.array, **kwargs) -> list[int]:
     }
 
     for i, keypoint in enumerate(query_locations):
-        if i in config:
+        if i in config and keypoint[0] >= 1 and keypoint[1] >= 1:
             pckh[config[i] * 3] = keypoint[0]
             pckh[config[i] * 3 + 1] = keypoint[1]
             pckh[config[i] * 3 + 2] = 1
